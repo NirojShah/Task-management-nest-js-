f4f99faa974f938f16bd03f639177841bc9f43d4
Niraj Shah <nirajshah950@gmail.com>
2026-01-05 23:11:03 +0530

    Define the Task Service functions

---
+++ src/task/task.interface.ts
@@ -0,0 +1,12 @@
+import { ResponseDto } from "src/response/response.dto";
+import { Tasks } from "./task.entity";
+import { CreateTaskDto } from "./task dto/task.dto";
+
+export interface TaskInterface{
+    createTasks(createTaskDto: CreateTaskDto):Promise<ResponseDto<Tasks>>;
+    updateTasks(): Promise<ResponseDto<any>>
+    deleteTask(): Promise<ResponseDto<any>>
+    getTasks(): Promise<ResponseDto<Tasks[]>>
+    getTaskById(taskId: number): Promise<ResponseDto<Tasks>>
+    updateTaskStatusById(taskId: number,status: string): Promise<ResponseDto<any>>
+}
