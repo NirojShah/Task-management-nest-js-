3497acbd083d38e343ee2a8b03bec9077c5ec9e1
Niraj Shah <nirajshah950@gmail.com>
2026-01-05 23:11:17 +0530

    Define the task table.

---
+++ src/task/task.entity.ts
@@ -0,0 +1,36 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { TaskStatus } from './enums/taskStatus.enum';
+
+@Entity('tasks')
+export class Tasks {
+  @PrimaryGeneratedColumn()
+  taskId: number;
+
+  @Column()
+  taskName: string;
+
+  @Column({ type: 'text' })
+  taskDescription: string;
+
+  @Column()
+  assignedBy: number;
+
+  @Column({ type: 'date' })
+  startDate: Date;
+
+  @Column({ type: 'date' })
+  endDate: Date;
+
+  @Column()
+  hours: number;
+
+  @Column({
+    type:"enum",
+    enum:TaskStatus,
+    default: TaskStatus.PENDING
+  })
+  status: TaskStatus
+
+  @Column()
+  userId: number
+}
