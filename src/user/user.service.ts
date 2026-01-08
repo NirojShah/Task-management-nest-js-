dfe0cb2ecf3305aef621386ce7e7d9a3005ddf63
Niraj Shah <nirajshah950@gmail.com>
2026-01-08 23:38:53 +0530

    User Service - update
    1. Implement Bcrypt to hash the password before storing in the db.
    
    2. use bcrypt to compare the hashed password.

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -3,9 +3,15 @@ import { UserInterface } from './user.interface';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
 import { CreateUserDto, LoginDto } from './user dto/user.dto';
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ResponseDto } from 'src/response/response.dto';
+import * as bcrypt from 'bcrypt';
+
 @Injectable()
 export class UserService implements UserInterface {
   constructor(
@@ -25,7 +31,19 @@ export class UserService implements UserInterface {
     if (!newUser) {
       throw new BadRequestException('Benki bitt');
     }
-    const savedUser = await this.userRepository.save(newUser);
+    const saltRound: number = 10;
+    const hashedPassword: string = await bcrypt.hash(
+      createUserDto.password,
+      saltRound,
+    );
+    const savedUser = await this.userRepository.save({
+      email: createUserDto.email,
+      firstName: createUserDto.firstName,
+      gender: createUserDto.gender,
+      lastName: createUserDto.lastName,
+      phoneNo: createUserDto.phoneNo,
+      password: hashedPassword,
+    });
 
     const userInfo = await this.userRepository.findOne({
       where: { email: createUserDto.email },
@@ -38,14 +56,23 @@ export class UserService implements UserInterface {
   }
 
   async login(loginDto: LoginDto): Promise<ResponseDto<any>> {
-    if(loginDto.email == undefined || loginDto.password == undefined){
-      throw new BadRequestException("Please send email and password.")
+    if (loginDto.email == undefined || loginDto.password == undefined) {
+      throw new BadRequestException('Please send email and password.');
     }
     const user = await this.userRepository.findOne({
-      where: { email: loginDto.email, password: loginDto.password },
+      where: { email: loginDto.email },
     });
     if (!user) {
       throw new BadRequestException('Invalid Credentials.');
+    }
+
+    const isValidPassword = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+    if (!isValidPassword) {
+      console.log("i am the one")
+      throw new UnauthorizedException('Invalid Credentials');
     }
     const payLoad = {
       firstName: user.firstName,
