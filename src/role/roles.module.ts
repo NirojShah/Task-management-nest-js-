718c171032e6b397701f16cdc61bf23f41e140fa
Niraj Shah <nirajshah950@gmail.com>
2026-01-10 21:55:53 +0530

    Role Module
    import all the required Entity

--- src/role/roles.module.ts
+++ src/role/roles.module.ts
@@ -4,9 +4,14 @@ import { Roles } from './role.entity';
 import { RoleController } from './role.controller';
 import { RolesService } from './role.service';
 import { JwtModule } from '@nestjs/jwt';
+import { User } from 'src/user/user.entity';
+import { RoleAssign } from './role.assign.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Roles]), JwtModule],
+  imports: [
+    TypeOrmModule.forFeature([Roles, User, RoleAssign]), // ✅ ALL entities used
+    JwtModule,
+  ],
   controllers: [RoleController],
   providers: [RolesService],
 })
