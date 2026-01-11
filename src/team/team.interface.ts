25283a1563129dea488de3b9dc0e7c2567f0284f
Niraj Shah <nirajshah950@gmail.com>
2026-01-11 22:16:52 +0530

    In Argument accept the AddMemberDtop

--- src/team/team.interface.ts
+++ src/team/team.interface.ts
@@ -1,9 +1,9 @@
 import { ResponseDto } from "src/response/response.dto";
-import { CreateTeamDto } from "./team.dto";
+import { AddTeamMemberDto, CreateTeamDto } from "./team.dto";
 
 export interface TeamInterface{
     createTeam(createTeamDto: CreateTeamDto): Promise<ResponseDto<any>>
-    addMember(): Promise<ResponseDto<any>>
+    addMember(addMemberDto: AddTeamMemberDto): Promise<ResponseDto<any>>
     assignRole(): Promise<ResponseDto<any>>
     removeMember(): Promise<ResponseDto<any>>
     updateTeam(): Promise<ResponseDto<any>>
