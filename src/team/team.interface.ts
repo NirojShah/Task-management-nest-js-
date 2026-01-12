8a6307562fd55745e8cbf70b8d298c239b656173
Niraj Shah <nirajshah950@gmail.com>
2026-01-12 21:33:06 +0530

    Team Interface - update 1. use UpdateTeamDto in the parameter.

--- src/team/team.interface.ts
+++ src/team/team.interface.ts
@@ -1,5 +1,5 @@
 import { ResponseDto } from 'src/response/response.dto';
-import { AddTeamMemberDto, CreateTeamDto } from './team.dto';
+import { AddTeamMemberDto, CreateTeamDto, UpdateTeamDto } from './team.dto';
 
 export interface TeamInterface {
   createTeam(createTeamDto: CreateTeamDto): Promise<ResponseDto<any>>;
@@ -7,5 +7,5 @@ export interface TeamInterface {
   addMember(addMemberDto: AddTeamMemberDto): Promise<ResponseDto<any>>;
   assignRole(): Promise<ResponseDto<any>>;
   removeMember(): Promise<ResponseDto<any>>;
-  updateTeam(): Promise<ResponseDto<any>>;
+  updateTeam(updateTeamDto: UpdateTeamDto): Promise<ResponseDto<any>>;
 }
