4039b2009bc7e5b6281be6f5e64b85b7e759db9d
Niraj Shah <nirajshah950@gmail.com>
2026-01-12 21:53:32 +0530

    Team Interface (feat) :
    accept RemoveMemberDTO in the removeMember function parameters.

--- src/team/team.interface.ts
+++ src/team/team.interface.ts
@@ -1,11 +1,16 @@
 import { ResponseDto } from 'src/response/response.dto';
-import { AddTeamMemberDto, CreateTeamDto, UpdateTeamDto } from './team.dto';
+import {
+  AddTeamMemberDto,
+  CreateTeamDto,
+  RemoveMemberDto,
+  UpdateTeamDto,
+} from './team.dto';
 
 export interface TeamInterface {
   createTeam(createTeamDto: CreateTeamDto): Promise<ResponseDto<any>>;
   searchTeam(name?: string): Promise<ResponseDto<any>>;
   addMember(addMemberDto: AddTeamMemberDto): Promise<ResponseDto<any>>;
   assignRole(): Promise<ResponseDto<any>>;
-  removeMember(): Promise<ResponseDto<any>>;
+  removeMember(removeMemberDto: RemoveMemberDto): Promise<ResponseDto<any>>;
   updateTeam(updateTeamDto: UpdateTeamDto): Promise<ResponseDto<any>>;
 }
