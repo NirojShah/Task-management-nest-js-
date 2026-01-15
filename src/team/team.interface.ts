c603484b9fc01c656e6cb1b517dcd1afa7f42294
Niraj Shah <nirajshah950@gmail.com>
2026-01-15 22:54:43 +0530

    Teeam Interface - pass the teamrole dto in assign role as a parameter

--- src/team/team.interface.ts
+++ src/team/team.interface.ts
@@ -1,6 +1,7 @@
 import { ResponseDto } from 'src/response/response.dto';
 import {
   AddTeamMemberDto,
+  AssignRoleDto,
   CreateTeamDto,
   RemoveMemberDto,
   UpdateTeamDto,
@@ -16,7 +17,7 @@ export interface TeamInterface {
     task: boolean,
   ): Promise<ResponseDto<any>>;
   addMember(addMemberDto: AddTeamMemberDto): Promise<ResponseDto<any>>;
-  assignRole(): Promise<ResponseDto<any>>;
+  assignRole(assignRoleDto: AssignRoleDto): Promise<ResponseDto<any>>;
   removeMember(removeMemberDto: RemoveMemberDto): Promise<ResponseDto<any>>;
   updateTeam(updateTeamDto: UpdateTeamDto): Promise<ResponseDto<any>>;
 }
