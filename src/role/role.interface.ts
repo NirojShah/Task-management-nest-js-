ef7e73f0fd39f1e677b23772fb3d901491b291e0
Niraj Shah <nirajshah950@gmail.com>
2026-01-18 21:58:49 +0530

    Role interface - update Accept the RoleUpdate DTO as an parameter.

--- src/role/role.interface.ts
+++ src/role/role.interface.ts
@@ -1,9 +1,9 @@
 import { ResponseDto } from 'src/response/response.dto';
-import { CreateRole } from './role.dto';
+import { CreateRole, UpdateRoleDTO } from './role.dto';
 
 export interface RolesInterface {
   createRole(createRolesDto: CreateRole): Promise<ResponseDto<any>>;
-  updateRole(updateRole: any): Promise<ResponseDto<any>>;
+  updateRole(updateRole: UpdateRoleDTO): Promise<ResponseDto<any>>;
   getRoles(): Promise<ResponseDto<any>>;
   deleteRole(roleId: number): Promise<ResponseDto<any>>;
   assignRole(userId: number, roleId: number): Promise<ResponseDto<any>>;
